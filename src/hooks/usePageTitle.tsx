d1ba87a27fb9ef002dc500019da10f1d7eac3dd1
yongjiajie <yjjyongjiajie@gmail.com>
2022-04-09 02:56:31 +0800

    docs(usePageTitle): add function doc

--- src/hooks/usePageTitle.tsx
+++ src/hooks/usePageTitle.tsx
@@ -1,5 +1,10 @@
 import { useEffect, useState } from "react";
 
+/**
+ * Hook used to set title of pages.
+ *
+ * @param pageTitle Title of page.
+ */
 const usePageTitle = (pageTitle?: string) => {
   const prefix = "yongjiajie";
   const [title, setTitle] = useState(pageTitle ?? "yongjiajie");
